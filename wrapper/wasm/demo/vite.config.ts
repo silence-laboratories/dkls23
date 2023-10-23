cb713f81f8459cd37ae1701d28c491849fbfc063
Artem Mironov <artem.mironov@gmail.com>
2023-10-23 21:20:15 +0200

    Simplify local tests and improve README a bit
    - provide set of keys pairs
    - update docker-compose.yaml to use provided key pairs
    - update dkg-setup.sh and dsg-setup.sh to use provided key pairs

--- wrapper/wasm/demo/vite.config.ts
+++ wrapper/wasm/demo/vite.config.ts
@@ -1,6 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 import { proxy } from './proxy';
+// import { proxy } from './proxy-local';
 
 export default defineConfig({
     plugins: [sveltekit()],
@@ -17,26 +18,5 @@ export default defineConfig({
         },
 
         proxy
-        // proxy: {
-        //     '^/party-0/.*': {
-        //         target: 'http://dkls-party-0.process.sl-demo.internal:8080',
-        //         rewrite: (path) => path.replace(/^\/party-0/, '')
-        //     },
-
-        //     '^/party-1/.*': {
-        //         target: 'http://dkls-party-1.process.sl-demo.internal:8080',
-        //         rewrite: (path) => path.replace(/^\/party-1/, '')
-        //     },
-
-        //     '^/party-2/.*': {
-        //         target: 'http://dkls-party-2.process.sl-demo.internal:8080',
-        //         rewrite: (path) => path.replace(/^\/party-2/, '')
-        //     },
-
-        //     '/v1/msg-relay': {
-        //         target: 'ws://msg-relay.process.sl-demo.internal:8080',
-        //         ws: true
-        //     }
-        // }
     }
 });
