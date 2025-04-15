febb9935a55749b2aae1ff30b576f5edb1df96a8
github-action-benchmark <github@users.noreply.github.com>
2025-04-15 20:28:32 +0000

    add Rust Benchmark (cargo) benchmark result for 94cb7931f64ee5926e7b6a05f132e03b81c14971

---
+++ docs/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1744748912893,
+  "repoUrl": "https://github.com/silence-laboratories/dkls23",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "leontiad@gmail.com",
+            "name": "Iraklis Leontiadis",
+            "username": "leontiad"
+          },
+          "committer": {
+            "email": "leontiad@gmail.com",
+            "name": "Iraklis Leontiadis",
+            "username": "leontiad"
+          },
+          "distinct": true,
+          "id": "94cb7931f64ee5926e7b6a05f132e03b81c14971",
+          "message": "polish page",
+          "timestamp": "2025-04-15T23:25:44+03:00",
+          "tree_id": "85a918c54c8f71cef1ba2cba97b242a0a1d38fc7",
+          "url": "https://github.com/silence-laboratories/dkls23/commit/94cb7931f64ee5926e7b6a05f132e03b81c14971"
+        },
+        "date": 1744748912146,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "dkg-2x2",
+            "value": 154893762,
+            "range": "± 9743525",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "dsg-2x2",
+            "value": 12164777,
+            "range": "± 394495",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
