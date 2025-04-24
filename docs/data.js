a7f56b7a815d00140abdf54f95c5d0daaea44c10
github-action-benchmark <github@users.noreply.github.com>
2025-04-24 22:36:00 +0000

    add Rust Benchmark (cargo) benchmark result for f0633d7fbf7b9cdb5d93772c9c39ba7ac8d6c8cf

--- docs/data.js
+++ docs/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745533885456,
+  "lastUpdate": 1745534160695,
   "repoUrl": "https://github.com/silence-laboratories/dkls23",
   "entries": {
     "Rust Benchmark": [
@@ -699,6 +699,38 @@ window.BENCHMARK_DATA = {
             "name": "dsg-2x2",
             "value": 9872565,
             "range": "± 1640324",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "silence-laboratories",
+            "username": "silence-laboratories"
+          },
+          "committer": {
+            "name": "silence-laboratories",
+            "username": "silence-laboratories"
+          },
+          "id": "f0633d7fbf7b9cdb5d93772c9c39ba7ac8d6c8cf",
+          "message": "Examples and rustdocs",
+          "timestamp": "2025-04-17T09:48:27Z",
+          "url": "https://github.com/silence-laboratories/dkls23/pull/1/commits/f0633d7fbf7b9cdb5d93772c9c39ba7ac8d6c8cf"
+        },
+        "date": 1745534159560,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "dkg-2x2",
+            "value": 136107555,
+            "range": "± 11057647",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "dsg-2x2",
+            "value": 9860676,
+            "range": "± 1671868",
             "unit": "ns/iter"
           }
         ]
