58f7bf3132e92b1023e783f56d0e09afa5724120
leontiad <leontiad@users.noreply.github.com>
2025-04-25 06:08:39 +0000

    Deploying to gh-pages from @ silence-laboratories/dkls23@3994e1881e0f756fdc1dac1a08a8b254b26dfc79 🚀

--- rustdocs/trait.impl/sl_mpc_mate/coord/trait.Relay.js
+++ rustdocs/trait.impl/sl_mpc_mate/coord/trait.Relay.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["dkls23",[]],["sl_mpc_mate",[]]]);
+    var implementors = Object.fromEntries([["dkls23",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[13,19]}
+//{"start":57,"fragment_lengths":[13]}
