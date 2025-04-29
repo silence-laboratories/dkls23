49b40d8fe1f1c03fb68e3e02147088b18ae46b63
Rhonstin <rhonstin@gmail.com>
2025-04-29 14:31:44 +0000

    Deploying to gh-pages from @ silence-laboratories/dkls23@7d7cacd214b5a59db72912729f3080346a4171a9 🚀

---
+++ docs/trait.impl/bytemuck/no_uninit/trait.NoUninit.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["dkls23",[["impl NoUninit for <a class=\"struct\" href=\"dkls23/sign/struct.PreSign.html\" title=\"struct dkls23::sign::PreSign\">PreSign</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[146]}
---
+++ docs/trait.impl/core/error/trait.Error.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["dkls23",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"dkls23/key_export/enum.KeyExportError.html\" title=\"enum dkls23::key_export::KeyExportError\">KeyExportError</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"dkls23/keygen/types/enum.KeygenError.html\" title=\"enum dkls23::keygen::types::KeygenError\">KeygenError</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"dkls23/sign/enum.SignError.html\" title=\"enum dkls23::sign::SignError\">SignError</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[839]}
